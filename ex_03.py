2194ff218cd979c36a700178c0f78121e8c35ffe
serg-svy <97796536+serg-svy@users.noreply.github.com>
2023-06-01 08:02:50 +0300

    upp

--- ex_03.py
+++ ex_03.py
@@ -2,12 +2,9 @@ lorem = "Amet jfnfnnn  jhfjkfdkdkkd jffkkff"
 
 lorem_up = lorem.upper()
 
-print(lorem_up)
 print(lorem)
 
-find_l = lorem.find('m'.upper())
+find_l_first = lorem.find('m'.upper())
+find_l_last = lorem.rfind('j'.upper())
 
-if find_l >= 0:
-    print(f"""Find in position {find_l}""")
-else:
-    print("Not find")    
+print(lorem[find_l_first:find_l_last]) 
---
+++ ex_04.py
@@ -0,0 +1,4 @@
+lorem = "Amet jfnfn\nnn  jhfjkfdkdk\nkd \njffkkff"
+
+split_data = [i.strip() for i in lorem.split("\n") ]
+print(split_data)
